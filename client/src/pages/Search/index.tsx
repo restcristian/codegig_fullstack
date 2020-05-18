cb5999c81db929a8b52a006713e31e4ff596a595
Cristian Restituyo <cristian.restituyo@denkwerk.com>
2020-05-18 13:26:01 +0200

    retrieve token to let user know that needs to be authenticated in order to search jobs

--- client/src/pages/Search/index.tsx
+++ client/src/pages/Search/index.tsx
@@ -1,10 +1,16 @@
 import React, { SyntheticEvent, FunctionComponent, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
+import { useSelector, shallowEqual } from "react-redux";
+import { AppStateType } from "../../store/types";
+import { isEmpty } from "../../helpers";
 
 interface Props extends RouteComponentProps {}
 
 const Search: FunctionComponent<Props> = ({ history }) => {
   const [term, setTerm] = useState<string>("");
+  const {
+    userReducer: { token },
+  } = useSelector((state: AppStateType) => state, shallowEqual);
   const onSubmit = (e: SyntheticEvent) => {
     history.push(`/jobs?term=${term}`);
   };
@@ -22,6 +28,22 @@ const Search: FunctionComponent<Props> = ({ history }) => {
         />
         <input type="submit" value="Search" />
       </form>
+      {isEmpty(token) && (
+        <div>
+          <span
+            style={{
+              display: "inline-block",
+              padding: "24px",
+              background: "red",
+              fontWeight: "bold",
+              color: "#fff",
+              marginTop: "12px",
+            }}
+          >
+            User Should be authenticated
+          </span>
+        </div>
+      )}
     </section>
   );
 };
